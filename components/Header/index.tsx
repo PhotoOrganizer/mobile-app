6a2b5be4b3493094a0fc7f8a7103d003f8edd9dc
Vaibhav <varora1406@gmail.com>
2021-09-06 00:20:35 +0530

    refactor: import complete logo and remove text part

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import GoogleLogo from '../../assets/google-logo.svg';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -12,19 +12,13 @@ const header: any = {
     paddingTop: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    left: 20
 }
 
 function Header() {
     return (
         <SafeAreaView edges={['top', 'bottom', 'left', 'right']}>
             <View style={header}>
-                <GoogleLogo style={{ marginRight: 85 }} />
-            </View>
-            <View style={{ ...header, left: 50, paddingTop: 0, top: -3 }}>
-                <Text style={{ fontSize: 26, fontWeight: '600', color: '#636466' }}>
-                    Photos
-                </Text>
+                <GoogleLogo />
             </View>
         </SafeAreaView>
     )
